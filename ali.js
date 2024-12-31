927cb7b8152a9aede6de08843c68a14c7a832039
Wasif Ali <wasifdaliali@gmail.com>
2024-12-31 23:39:30 +0530

    add push array done

---
+++ ali.js
@@ -0,0 +1,12 @@
+let a=[]; // empty array
+let b= new Array() // empty array.
+
+//push
+
+cars= ['Maruti', 'hyundai,'];
+cars.push('Kia', 'Tata','Mahindra'); 
+console.log(cars); // [Maruti', 'hyundai,'Kia', 'Tata']
+
+cars= ['Maruti', 'hyundai,'];
+cars.push('Kia', 'Tata','Mahindra'); 
+console.log(cars); // [Maruti', 'hyundai,'Kia', 'Tata']
